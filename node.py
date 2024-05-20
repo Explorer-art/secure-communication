4042ce983a6774babaaaf5831e16893549e0d8fc
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2024-05-20 23:37:09 +0300

    Update node.py

--- node.py
+++ node.py
@@ -13,14 +13,11 @@ from config import *
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Cipher import PKCS1_OAEP
 
-port = 27575
+port = server_port
 
-number_maximum_length = 64
-
-numbers = []
 clients = []
-addresses = []
-numbers_connect = []
+
+cache = []
 
 def clear():
 	if sys.platform == "win32":
@@ -82,19 +79,6 @@ def get_route_list():
 	
 	return route_list
 
-def get_banned_ips():
-	banned_ips = []
-
-	file = open("banned_ips.txt", "r")
-	lines = file.readlines()
-	file.close()
-
-	for line in lines:
-		banned_ip = line.replace("\n", "")
-		banned_ips.append(banned_ip)
-	
-	return banned_ips
-
 def get_now_date():
 	now = datetime.datetime.now()
 	return f"{now.day}-{now.month}-{now.year} {now.hour}:{now.minute}:{now.second}"
@@ -107,11 +91,13 @@ def get_ip():
 		return False
 
 def log(message):
+	now_date = get_now_date()
+
 	if out_logging == True:
-		print("[LOG] [" + get_now_date() + "] " + message)
+		print("[LOG] [" + now_date + "] " + message)
 
 	file = open("log.txt", "a")
-	file.write("[LOG] [" + get_now_date() + "] " + message + "\n")
+	file.write("[LOG] [" + now_date + "] " + message + "\n")
 	file.close()
 
 def update_route_list(ip, port):
@@ -134,29 +120,17 @@ def add_node(address):
 	file.write(f"{address[0]}:{address[1]} " + datetime.datetime.now())
 	file.close()
 
-def send_node(ip, number, mode, message):
-	try:
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		client.connect((ip, port))
+def check_route_list():
+	while True:
+		route_list = []
 
-		data = "REQUEST=SEND_NODE:" + mode
+		file = open("route_list.txt", "r")
+		lines = file.readlines()
+		file.close()
 
-		client.send(data.encode("utf-8"))
-
-		client.send(number.encode("utf-8"))
-
-		client.send(message)
-
-		data = client.recv(1024)
-
-		client.close()
-
-		if data == "REQUEST=OKAY":
-			return True
-		elif data == "REQUEST=NOT_FOUND":
-			return False
-	except:
-		return False
+		for line in lines:
+			line = line.replace("\n", "")
+			route_list.append(line)
 
 def kick(client, address, reason):
 	data = "REQUEST=KICKED:" + reason
@@ -165,44 +139,14 @@ def kick(client, address, reason):
 
 	log(f"{address[0]}:{address[1]} kicked for a reason: " + reason)
 
-def ban(client, address, reason):
-	file = open("banned_ip.txt", "a")
-	file.write(address[0] + "\n")
-	file.close()
-
-	kick(client, address, "You have been banned!")
-
-	log(f"{address[0]}:{address[1]} banned for a reason: " + reason)
-
-def amount(): #Количество подключённых пользователей
-	amount = 0
-
-	for client in clients:
-		amount += 1
-
-	return amount
-
-def list(): #Список подключённых пользователей
-	for number in numbers:
-		log(number + ", ", end="")
-
-def send_message(message, number_connect): #Отправка сообщения отдельному пользователю
-	try:
-		if number_connect in numbers:
-			number_connect_index = numbers.index(number_connect)
-			client_data = clients[number_connect_index]
-			client_data.send(message)
-	except Exception as error:
-		print(error)
-
-def send_all_clients(data): #Отправка сообщения всем пользователям
-	for client in clients:
-		client.send(data)
-
 def send_all_nodes(data):
 	nodes = get_route_list()
-	for ip in nodes:
-		if ip == server_ip:
+	for node in nodes:
+		node_splited = node.split(":")
+		ip = node_splited[0]
+		port = int(node_splited[1])
+
+		if ip == server_ip and port == server_port:
 			continue
 
 		try:
@@ -212,6 +156,10 @@ def send_all_nodes(data):
 			client.close()
 		except:
 			log(f"{ip}:{port} connect failed")
+
+def send_all_clients(data): #Отправка сообщения всем пользователям
+	for client in clients:
+		client.send(data)
 
 def handle(client, address): #Пользователь
 	while True:
@@ -232,18 +180,6 @@ def handle(client, address): #Пользователь
 			log(f"{address[0]}:{address[1]} disconnected")
 			break
 
-def check_route_list():
-	while True:
-		route_list = []
-
-		file = open("route_list.txt", "r")
-		lines = file.readlines()
-		file.close()
-
-		for line in lines:
-			line = line.replace("\n", "")
-			route_list.append(line)
-
 def server_handle(ip, port):
 	global clients
 	log("Started server..")
@@ -262,9 +198,15 @@ def server_handle(ip, port):
 		log(f"{address[0]}:{address[1]} connected ")
 
 		try:
-			data = client.recv(1024).decode("utf-8")
+			data = client.recv(1024)
 
-			if data == "REQUEST=USER_CONNECT":
+			if len(data) > 35 and not data in cache:
+				send_all_clients(data)
+				send_all_nodes(data)
+				cache.append(data)
+				continue
+
+			if data.decode("utf-8") == "REQUEST=USER_CONNECT":
 				if client in clients:
 					log(f"{address[0]}:{address[1]} already connected")
 					kick(client, address, "You already connected!")
@@ -274,16 +216,15 @@ def server_handle(ip, port):
 				clients.append(client)
 
 				threading.Thread(target=handle, daemon=True, args=(client, address)).start()
-			elif data == "REQUEST=GET_ROUTE_LIST":
+			elif data.decode("utf-8") == "REQUEST=GET_ROUTE_LIST":
 				send_file(client, "route_list.txt")
 				log(f"{address[0]}:{address[1]} get route list")
 				client.close()
-			elif data == "REQUEST=ADD_NODE":
+			elif data.decode("utf-8") == "REQUEST=ADD_NODE":
 				log(f"{address[0]}:{address[1]} add node")
 				add_node(address)
-		except UnicodeDecodeError:
-			send_all_clients(data)
-			send_all_nodes(data)
+		except:
+			pass
 
 def encrypt(public_key, message):
 	key = RSA.import_key(public_key)
@@ -302,21 +243,23 @@ def receive(client, private_key):
 		try:
 			message = decrypt(private_key, client.recv(1024)).decode("utf-8")
 
+			message = message.split("//")[0]
+
 			print("\n" + "[" + get_now_date() + "] " + message + "\n")
-		except Exception as error:
-			print("Error! " + str(error))
+		except:
+			pass
 
 def writer(client, public_key):
 	while True:
 		message = input("\n> ")
 
 		if message == "!info":
-			print("Информация:")
-			print("Версия: 1.0")
+			print("Information:")
+			print("Version: 1.0")
 			print("")
 
 		if message != "":
-			message = f"{username} > {message}"
+			message = f"{time.time()}//{username} > {message}"
 
 			print("[" + get_now_date() + "] " + message)
 
@@ -332,21 +275,6 @@ if os.path.isfile("log.txt") == False:
 
 clear()
 
-if os.path.isfile("public_key.txt") == False or os.path.isfile("private_key.txt") == False and client_switch == True:
-	print("[LOG] Generating encryption keys..")
-
-	key = RSA.generate(2048)
-	public_key = key.publickey().export_key()
-	private_key = key.export_key()
-
-	file = open("public_key.txt", "wb")
-	file.write(public_key)
-	file.close()
-
-	file = open("private_key.txt", "wb")
-	file.write(private_key)
-	file.close()
-
 if server_switch == True:
 	if server_ip == "":
 		server_ip = get_ip()
@@ -360,11 +288,18 @@ route_list = get_route_list()
 if auto_update_route_list == True:
 	print("[LOG] Update route list")
 
-	ip_route = random.choice(route_list)
+	if ip_connect_node == "":
+		route = random.choice(route_list)
+		route_splited = route.split(":")
+		ip = route_splited[0]
+		port = int(route_splited[1])
+	else:
+		ip = ip_connect_node
+		port = port_connect_node
 
-	print(f"[LOG] Connecting to {ip_route}:{port}..")
+	print(f"[LOG] Connecting to {ip}:{port}..")
 
-	p = update_route_list(ip_route, port)
+	p = update_route_list(ip, port)
 
 	if p == True:
 		print("[LOG] Updated route list")
@@ -382,10 +317,30 @@ if client_switch == True:
 
 		print("[LOG] Server started")
 
+	username = input("Enter username: ")
+
+	if os.path.exists("private_key.txt") == False:
+		print("[LOG] Generating encryption keys..")
+
+		key = RSA.generate(2048)
+		public_key = key.publickey().export_key()
+		private_key = key.export_key()
+
+		file = open(username + ".txt", "wb")
+		file.write(public_key)
+		file.close()
+
+		file = open("private_key.txt", "wb")
+		file.write(private_key)
+		file.close()
+
+	if os.path.exists(username + ".txt") == False:
+		print(f"[ERROR] Username {username} not found!")
+		sys.exit()
+
 	if os.path.exists("friends") == False:
 		os.mkdir("friends")
 
-	username = input("Enter username: ")
 	friend_username = input("Enter username friend: ")
 
 	try:
@@ -401,13 +356,20 @@ if client_switch == True:
 
 	if ip_connect_node == "":
 		if auto_node_select == True:
-			ip_node = random.choice(route_list)
+			route = random.choice(route_list)
+			route_splited = route.split(":")
+			ip_node = route_splited[0]
+			port_node = int(route_splited[1])
 		elif auto_node_select == False:
-			ip_node = input("Enter the IP address of the server you want to connect: ")
+			node = input("Enter the IP address of the server you want to connect: ")
+			node_splited = node.split(":")
+			ip_node = node_splited[0]
+			port_node = int(node_splited[1])
 	else:
 		ip_node = ip_connect_node
+		port_node = port_connect_node
 
-	print(f"[LOG] Connecting to {ip_node}:{port}..")
+	print(f"[LOG] Connecting to {ip_node}:{port_node}..")
 
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect((ip_node, port))
@@ -436,11 +398,11 @@ if client_switch == True:
 
 	clear()
 
-	print("Welcome to Secure Communication!")
-	print("")
-	print("Your username: " + username)
-	print("Friend username: " + friend_username)
-	print("")
+	print(f"""
+		Welcome to Secure Communication!
+
+		Your username: {username}
+		Friend username: {friend_username}""")
 
 	receive_thread = threading.Thread(target=receive, daemon=True, args=(client, private_key))  # Получение всех сообщений
 	receive_thread.start()
